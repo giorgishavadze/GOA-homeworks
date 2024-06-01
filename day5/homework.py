b4ace75095f140b55bdaeffc99f19bc64c58c2cb
giorgishavadze <gshavadze2009@gmail.com>
2024-06-01 18:55:54 +0400

    sixth commit

---
+++ day5/homework.py
@@ -0,0 +1,34 @@
+name=input("please enter the deliver's name and surname here:")
+letter=input("please enter your letter here:")
+recipient=input("please enter recipient name and surname:")
+print(name)
+print(letter)
+print(recipient)
+
+
+great_grandmother=91
+mother=43
+father=44
+sister1=14
+sister2=12
+sister3=7
+sister4=6
+brother=4
+
+years=20
+
+print(great_grandmother + years)
+print(mother + years)
+print(father + years)
+print(sister1 + years)
+print(sister2 + years)
+print(sister3 + years)
+print(sister4 + years)
+print(brother + years)
+
+name=input("please enter your name:")
+surname=input("please enter your surname:")
+age=input("please enter your age:")
+address=input("please enter your address:")
+gmail=input("please enter your gmail:")
+print(name + " " + surname + " " + age + " " + address + " " + gmail)
