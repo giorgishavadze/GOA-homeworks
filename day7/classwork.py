76f16119dce9b0308cc147e8d28fd7f65e84f93f
giorgishavadze <gshavadze2009@gmail.com>
2024-06-06 13:04:53 +0400

    eighth commit

---
+++ day7/classwork.py
@@ -0,0 +1,7 @@
+age = int(input("please enter your age"))
+
+print(age >= 18 or age <= 25)
+
+budget = int(input("please enter your budget"))
+
+print(budget > 50 or budget > 100)
---
+++ day7/homework.py
@@ -0,0 +1,28 @@
+num1 = int(input("please enter number:"))
+num2 = int(input("please enter number:"))
+num3 = int(input("please enter number:"))
+num4 = int(input("please enter number:"))
+num5 = int(input("please enter number:"))
+
+print(num1 + num2 + num3 + num4 + num5)
+print(num1 - num2 - num3 - num4 - num5)
+print(num1 * num2 * num3 * num4 * num5)
+print(num1 / num2 / num3 / num4 / num5)
+print(num1 % num2 % num3 % num4 % num5)
+print(num1 // num2 // num3 // num4 // num5)
+
+age = int(input("please enter your age"))
+
+print(age > 18 and age < 20)
+
+print(10 > 5)
+print(5 < 10)
+print(10 >= 5)
+print(5 <= 10)
+print(10 != 5)
+print(5 == 5)
+
+num1 = int(input("please enter number"))
+num2 = float(input("please enter number"))
+
+print(num1 > num2)
