c925898fc33a97f9bbd11ce94ea68d0157bcf3d5
giorgishavadze <gshavadze2009@gmail.com>
2024-06-12 00:46:46 +0400

    seventh commit

---
+++ level06/classwork.py
@@ -0,0 +1,43 @@
+num1=int(input("please enter first number:"))
+num2=int(input("please enter second number:"))
+num3=int(input("please enter third number:"))
+num4=int(input("please enter forth number:"))
+
+print("result:",num1 + num2 + num3 + num4) #ერთმანეთს უნდა  მიუმატოს გამომყენებელმა 4 სხვადასხვა რიცხვი
+print("result:",num1 - num2 - num3 - num4) #აქ უნდაგამოაკლოს
+print("result:",num1 * num2 * num3 * num4) #აქ გაამრავლოს
+print("result:",num1 / num2 / num3 / num4) #აქ გაყოს
+
+num10=num1 + num2 + num3 + num4
+num11=num1 - num2 - num3 - num4
+num12=num1 * num2 * num3 * num4
+num13=num1 / num2 / num3 / num4
+
+print(num10 > num11)
+print(num12 > num13)
+print(num10 > num13)
+print(num12 > num11)
+print(num12 > num10)
+print(num13 > num11)
+print(num12 > (num10 + num11 + num13))
+
+#(input)_ი ნიშნავს შეტანას მაგ:კლავიატურა, მაუსი, ტელევიზორის პულტი.
+#(output)_ი ნიშნავს გატანას მაგ:ყურსასმენი, დინამიკი, რადიო.
+
+
+
+
+name=input("please enter your name:")
+last_name=input("please enter your last name:")
+age=int(input("please enter your age:"))
+email=input("please enter your email:")
+
+
+
+
+
+
+
+
+
+
